16ca2fdfaeaa1f638faebd84b03f1732c097afb3
Huarcaya <phuarcaya@gmail.com>
2023-03-29 07:53:51 -0500

    change home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { globalConstanst } from './shared/globals/global-constanst';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'AloDeskSite';
+  whatsAppChat = globalConstanst.whatsAppChat;
 }
--- src/app/shared/globals/global-constanst.ts
+++ src/app/shared/globals/global-constanst.ts
@@ -1,7 +1,7 @@
 export const globalConstanst = {
   phoneContact: "(+51) 954 138 326",
   emailContact: "devnet.ph.mkt@gmail.com",
-  whatsAppChat: "https://wa.me/51910209061",
+  whatsAppChat: "https://wa.me/51954138326",
   facebookPage: "https://www.facebook.com/alodesk",
   instagramPage: "https://www.facebook.com/alodesk",
   webSite: "https://www.alodesk.pe",
